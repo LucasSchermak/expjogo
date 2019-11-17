7e74e26dd98c0129dd9c99730a345befa3487fc0
LucasGschermak <lgschermak@gmail.com>
2019-11-17 01:50:26 -0300

    Update Porta.java

--- Porta.java
+++ Porta.java
@@ -1,1 +1,34 @@
+public class Porta extends Saida
+{
 
+    private boolean fechada;
+
+    /**
+     * Constructor for objects of class Door
+     */
+    public Porta(String direcao, Room saidaProxima, boolean fechada){
+        this.fechada = fechada;
+        this.setDirecao(direcao);
+        this.setSaidaProxima(saidaProxima);
+    }
+
+    public void fechar()
+    {
+        fechada = true;
+        return;
+    }
+
+    /**
+     * Unlocks this door.
+     */
+    public void abrir()
+    {
+        fechada = false;
+        return;
+    }
+
+    public boolean getFechada()
+    {
+        return fechada;
+    }
+}
