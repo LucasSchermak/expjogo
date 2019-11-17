dd3a984f9c4de1e31aad74fe2b16a5b06afe8699
Allyson Luan Dunke <57627628+alluke96@users.noreply.github.com>
2019-11-17 02:43:34 -0300

    Add files via upload

--- CriaSala.java
+++ CriaSala.java
@@ -28,34 +28,40 @@ public class CriaSala
         cozinha = new Room("na nossa cozinha", "tem muitas coisas que eu não posso mexer aqui. Engraçado, está tudo arrumado, achei que encontraria a mamãe aqui cozinhando. \nA porta para o jardim fica aqui");
         banheiro = new Room("no meu banheiro", "minha escova de dentes está ficando velha. Eu não estou com vontade de fazer pipi agora");
         garagem = new Room("na nossa garagem", "nosso carro azul está aqui, ele está sujo. Tem também as ferramentas do papai. \nO portão da garagem é automática");
-        quartoPais = new Room("no quarto do papai e da mamãe", "a cama deles é enorme, a porta da varanda está aberta");
+        quartoPais = new Room("no quarto do papai e da mamãe", "a cama deles é enorme, a porta da sacada está aberta");
         escadas = new Room("nas escadas de casa", "mamãe diz que aqui é perigoso");
         jardim = new Room("no jardim de casa", "as flores da mamãe estão crescendo muito bem");
         sacada = new Room("na sacada", "aqui é muito alto! Se o papai me ver aqui ele me mata!");
         c1 = new Room("no corredor de cima", "este corredor leva até o meu quarto e do meu irmão, o banheiro, o quarto dos meus pais e as escadas");
         c2 = new Room("no corredor de baixo", "este corredor leva até as escadas, a cozinha, a sala e a garagem");
 
-
-        rua = new Room("consegui sair de casa", "há varias outras casas no nosso condomínio, não há muitos carros na rua");
+        rua = new Room("consegui sair de casa", "há várias outras casas no nosso condomínio, não há muitos carros na rua");
+        
         // initialise room exits
-        minhaCasa.setExit("oeste", rua);
-        /*homeP.setExit("sul", hPcozinha);
-        homeP.setExit("oeste", hPquarto);
-        homeP.setExit("norte",rua);
-
-        hPsala.setExit("oeste", hPcozinha);
-        hPsala.setExit("oeste", hPquarto);
-        hPsala.setExit("norte", rua);
-
-        hPcozinha.setExit("leste", hPsala);
-        hPcozinha.setExit("oeste", hPquarto);
-
-        hPquarto.setExit("norte", hPsala);
-        hPquarto.setExit("leste", hPcozinha);
-
-        hPbanheiro.setExit("oeste", hPsala);
-        rua.setExit("sul", homeP);
-        */
+        minhaCasa.setExit("porta", rua);
+        
+        quarto.setExit("corredor", c1);
+        banheiro.setExit("corredor", c1);
+        quartoPais.setExit("corredor", c1);
+        quartoPais.setExit("sacada", sacada);
+        c1.setExit("quarto", quarto);
+        c1.setExit("banheiro", banheiro);
+        c1.setPorta("quarto dos pais", quartoPais, true);
+        c1.setExit("escadas", escadas);
+        escadas.setExit("subir", c1);
+        escadas.setExit("descer", c2); 
+        c2.setExit("escadas", escadas);
+        c2.setExit("cozinha", cozinha);
+        c2.setExit("sala", sala);
+        c2.setExit("garagem", garagem);
+        cozinha.setExit("corredor", c2);
+        cozinha.setExit("jardim", jardim);
+        sala.setExit("corredor", c2);
+        sala.setPorta("porta", rua, true);
+        garagem.setExit("porta", c2);
+        garagem.setPorta("portao", rua, true);
+        
+        
         setCurrentRoom(quarto);
     }
 
