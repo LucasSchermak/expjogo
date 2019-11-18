33bc53b278fe3c480738052c971ecad5b50c746f
Allyson Luan Dunke <57627628+alluke96@users.noreply.github.com>
2019-11-18 03:54:01 -0300

    Add files via upload

--- expcriativaFINAL/Game.java
+++ expcriativaFINAL/Game.java
@@ -57,9 +57,9 @@ public class Game
         //criar as salas
         // CASA
         minhaCasa = new Room("na minha casa","ela é a mais bonita do condominio");
-        quarto = new Room("no meu quarto", "aqui ficam meus brinquedos e minha cama. Meu irmão mais velho, Jonas, também dorme aqui, mas ele é chato. Sorte que ele está dormindo agora");
+        quarto = new Room("no meu quarto", "aqui ficam meus brinquedos e minha cama. \nMeu irmão mais velho, Jonas, também dorme aqui, mas ele é chato. Sorte que ele está dormindo agora");
         sala = new Room("na nossa sala", "a tv está ligada no One Piece, a guitarra do meu irmão está jogada no chão, como sempre...\nA porta para a rua fica aqui");
-        cozinha = new Room("na nossa cozinha", "tem muitas coisas que eu não posso mexer aqui. Engraçado, está tudo arrumado, achei que encontraria a mamãe aqui cozinhando. \nA porta para o jardim fica aqui");
+        cozinha = new Room("na nossa cozinha", "tem muitas coisas que eu não posso mexer aqui. \nEngraçado, está tudo arrumado, achei que encontraria a mamãe aqui cozinhando. \nA porta para o jardim fica aqui");
         banheiro = new Room("no meu banheiro", "minha escova de dentes está ficando velha. Eu não estou com vontade de fazer pipi agora");
         garagem = new Room("na nossa garagem", "nosso carro azul está aqui, ele está sujo. Tem também as ferramentas do papai. \nO portão da garagem é automática");
         quartoPais = new Room("no quarto do papai e da mamãe", "a cama deles é enorme, a porta da sacada está aberta");
@@ -401,7 +401,7 @@ public class Game
         }
     }
 
-public static void main(String []args) {
-        new Game().play();
-    }
+//public static void main(String []args) {
+ //       new Game().play();
+  //  }
 }
